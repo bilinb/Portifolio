f3077a5cbd27b978e54e018a4d2e67b5df0b583b
bilinb <70768642+bilinb@users.noreply.github.com>
2024-11-02 18:27:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+window.addEventListener('scroll', () => {
+    const elements = document.querySelectorAll('.section');
+    elements.forEach((element) => {
+        const position = element.getBoundingClientRect();
+        if(position.top < window.innerHeight && position.bottom >= 0) {
+            element.style.opacity = '1';
+            element.style.transform = 'translateY(0)';
+        } else {
+            element.style.opacity = '0';
+            element.style.transform = 'translateY(20px)';
+        }
+    });
+});
